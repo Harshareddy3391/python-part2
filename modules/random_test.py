a1b683a398d039b57cc536328ccff053f7999ef9
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-19 12:22:33 +0530

    msg

---
+++ modules/random_test.py
@@ -0,0 +1,13 @@
+import random
+
+#print(dir(random.random))
+
+"""
+for x in range(6):
+    print(random.randint(10,100))"""
+
+
+listt="harsha"
+
+for i in range(3):
+    print(random.choice(listt))
