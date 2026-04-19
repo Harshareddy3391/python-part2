3734e0e3df98bce51454d2713b8577a913e36fc9
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-19 18:58:13 +0530

    csv

--- filehandling/two.py
+++ filehandling/two.py
@@ -1,8 +1,8 @@
 about="""If you want, I can give:
 Real-time example where not closing file causes bug
-OR MCQs for interview practice"""
+OR MCQs for interview practice   y Responsibilities: • Attend and resolve machine breakdowns promptly to minimize downtime • Execute preventive maintenance activities as per schedule • Basic understanding and troubleshooting of control systems and mechanical components • Manage and track spare parts inventory to ensure availability • Support maintenance and monitoring of utility systems such as DG sets, compressors, and fire hydrant systems • Ensure proper functioning of plant lighting systems • Maintain records of maintenance activities and report issues proactively • Follow safety standards and ensure compliance during all maintenance activities Requirements: • 23 years of relevant maintenance experience in manufacturing/plant environment • Diploma / ITI in Electrical & Electronics • Hands-on experience in electrical maintenance and basic mechanical systems • Knowledge of u"""
 
-
+"""
 fp=open('da.text','w')
 newfile=fp.write(about)
 
@@ -12,7 +12,19 @@ print(newfile)
 
 fp.close()
 
+"""
+"""
+with open('da.text','r') as f:
+    filedataread=f.read()
 
+    print(filedataread)
 
+    f.close"""
 
-with open(da)
+fp=open('d.txt','w+')
+ 
+adddata=fp.write("\nhello good morging")
+data=fp.read()
+print(data)
+
+fp.close()
