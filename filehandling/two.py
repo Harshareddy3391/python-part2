be7e08a02ed3322792e8d59f0fed53a6def5647e
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-19 17:26:49 +0530

    new

--- filehandling/two.py
+++ filehandling/two.py
@@ -1,11 +1,18 @@
 about="""If you want, I can give:
-👉 Real-time example where not closing file causes bug
-👉 OR MCQs for interview practice"""
+Real-time example where not closing file causes bug
+OR MCQs for interview practice"""
 
 
 fp=open('da.text','w')
 newfile=fp.write(about)
 
+ 
+
 print(newfile)
 
 fp.close()
+
+
+
+
+with open(da)
