20df3e1bf464a704ea930e293128ce9e1576785d
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-21 13:00:58 +0530

    instance method

---
+++ oop/methods.py
@@ -0,0 +1,46 @@
+"""class Methods:
+    s=400
+    def __init__(self,name):
+        self.ur_name=name
+    def normal_method(self):
+        pass
+    @classmethod
+    def cls_method(cls):
+        pass
+    @staticmethod
+    def static_method():
+        pass
+
+a1=Methods("harsha")
+print(a1.__dict__)
+
+
+"""
+
+
+
+class Student:
+    def __init__(self,name):
+        self.user_name=name
+    def upname(self):
+        self.up_name +=9
+
+a1=Student(3)
+a1.upname()
+
+print(a1.__dict__)
+
+"""
+
+class Student:
+    def __init__(self, name):
+        self.user_name = name
+
+    def upname(self):
+        self.user_name += 9   # correct variable
+
+
+a1 = Student(3)
+a1.upname()
+
+print(a1.__dict__)"""
