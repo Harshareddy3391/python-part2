3a52e73717931444bf475d0964572d2779dff350
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-24 17:24:18 +0530

    errors

---
+++ errorhandling/one.py
@@ -0,0 +1,24 @@
+"""print(10/3)
+
+try :
+    print(10/0)
+
+except ZeroDivisionError :
+    print("edi error")
+
+
+print("excuting successfully")"""
+
+
+
+try:
+    a=int(input("enter a number :"))
+    b=int(input("enter a number :"))
+    print(a+b)
+
+
+except ValueError:
+
+    print("numbers enter chey bro")
+
+     
