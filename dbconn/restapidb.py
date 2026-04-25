c62a022726adbe30eecb2793be2c7fe9ec3f20ef
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-25 23:59:36 +0530

    f

--- dbconn/restapidb.py
+++ dbconn/restapidb.py
@@ -3,5 +3,20 @@ import requests
 
 
 
-responce=
+responce=requests.get("https://jsonplaceholder.typicode.com/users")
+
+json_data=responce.json()
+
+lst_data=[]
+
+for i in json_data:
+    lst_data.append((i['id'],i['name'],i['address']['zipcode'],i['phone'],i['email']))
+
+
+f=lambda x: x 
+
+print(f(lst_data))
+
+ 
+
 
