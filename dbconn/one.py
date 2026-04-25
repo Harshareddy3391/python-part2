b3f94e6d68a03f1b430174c5cca4def87d858674
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-25 16:21:18 +0530

    con

--- dbconn/one.py
+++ dbconn/one.py
@@ -2,3 +2,14 @@ import mysql.connector
 
 
 connction=mysql.connector.connect(host="localhost",user="root",password="Harsha@345")
+
+
+
+if connction.is_connected :
+
+    print("conncted successfully")
+
+
+else:
+    print("fail")
+
