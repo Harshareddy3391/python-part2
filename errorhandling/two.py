a2b43599b231065fb6bc1509191919d1b15cc523
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-25 16:13:05 +0530

    db

---
+++ errorhandling/two.py
@@ -0,0 +1,18 @@
+"""a=int(input("enter your age :"))
+
+
+if a>18 :
+    print("your eligible for vote")
+
+else:
+    print("you not eligible for vote...")
+
+
+"""
+
+
+
+age = 19
+
+if age < 18:
+    raise ValueError("User must be 18+ to register")
