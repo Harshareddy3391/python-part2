9ac3bcace97f877a76e980ad56d1fc6ea441567d
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-04-27 22:28:17 +0530

    j

--- dbconn/test.py
+++ dbconn/test.py
@@ -29,9 +29,13 @@ for i in range(6):
     print()"""
 
 """
-for i in range(6,1,-1):
+for i in range(6,0,-1):
     for j in range(i):
 
         print("*",end="")
 
     print()"""
+
+
+
+for i in range()
