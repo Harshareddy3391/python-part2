8f64d6661c0066aba307bb3beabb4c31a529fd9f
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-07-13 22:48:29 +0530

    add

--- oopjul2/emp.py
+++ oopjul2/emp.py
@@ -1,4 +1,5 @@
 class Employee:
+    n=12
     def __init__(self,name,last,pay):
         self.name=name
         self.last=last
@@ -17,4 +18,5 @@ print(e1.inc(20))
 e1.pa=21
 print(e1.__dict__)
 print(e1.full_name())
+print(Employee.n)
  
